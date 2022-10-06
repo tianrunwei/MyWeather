2a674420a16ec1c2d57736a0be711e64ab92105b
tianrw <2407478859@qq.com>
2022-10-06 16:03:32 +0800

    加入显示天气功能。

--- app/src/main/java/com/myweather/android/logic/network/SunnyWeatherNetwork.kt
+++ app/src/main/java/com/myweather/android/logic/network/SunnyWeatherNetwork.kt
@@ -9,12 +9,20 @@ import kotlin.coroutines.suspendCoroutine
 
 
 object SunnyWeatherNetwork {
+
+    private val weatherService = ServiceCreator.create(WeatherService::class.java)
+    suspend fun getDailyWeather(lng: String, lat: String) =
+        weatherService.getDailyWeather(lng, lat).await()
+
+    suspend fun getRealtimeWeather(lng: String, lat: String) =
+        weatherService.getRealtimeWeather(lng, lat).await()
+
     private val placeService = ServiceCreator.create<PlaceService>()
 
     suspend fun searchPlaces(query: String) = placeService.searchPlaces(query).await()
 
     private suspend fun <T> Call<T>.await(): T {
-        return suspendCoroutine {continuation ->
+        return suspendCoroutine { continuation ->
             enqueue(object : Callback<T> {
                 override fun onResponse(call: Call<T>, response: Response<T>) {
                     val body = response.body()
@@ -23,6 +31,7 @@ object SunnyWeatherNetwork {
                         RuntimeException("response body is null")
                     )
                 }
+
                 override fun onFailure(call: Call<T>, t: Throwable) {
                     continuation.resumeWithException(t)
                 }
